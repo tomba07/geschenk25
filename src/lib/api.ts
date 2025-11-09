b4468457e16a00d4c494ef7841d600f996f10106
Mirko Teschke <mirko.teschke@gmail.com>
2025-11-08 19:49:06 -0800

    Fix TS Issue

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -28,7 +28,7 @@ class ApiClient {
     };
 
     if (this.token) {
-      headers['Authorization'] = `Bearer ${this.token}`;
+      (headers as Record<string, string>)['Authorization'] = `Bearer ${this.token}`;
     }
 
     try {
