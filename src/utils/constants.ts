2c501da7bc270bc5e3307f4e5c758263d3699129
Mirko Teschke <mirko.teschke@gmail.com>
2025-11-21 18:59:26 -0800

    Add Missing File

---
+++ src/utils/constants.ts
@@ -0,0 +1,5 @@
+// App Store and Play Store URLs
+// TODO: Update with actual App Store ID once the app is published
+export const APP_STORE_URL = 'https://apps.apple.com/app/id6739000000'; // Replace with actual App Store ID
+export const PLAY_STORE_URL = 'https://play.google.com/store/apps/details?id=com.tomba07.geschenk25';
+
